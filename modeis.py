cb6c9a6ace7e6a375763ff6bd2a46c735d597ba2
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-02-27 13:28:14 +0200

    Update modeis.py

--- modeis.py
+++ modeis.py
@@ -10,3 +10,9 @@ class Quote(Document):
     tags = ListField(StringField())
     author = ReferenceField(Author)
     quote = StringField()
+
+class Contact(Document):
+    full_name = StringField(required=True)
+    email = StringField(required=True)
+    is_message_sent = BooleanField(default=False)
+
